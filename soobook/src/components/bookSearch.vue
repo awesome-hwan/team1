f82af08cbb640526fbf2266fb7888d5db141ded0
DESKTOP-ABVBI8V\hun08 <byeonghun08@gmail.com>
2017-04-17 18:21:16 +0900

    토글바 구현

--- soobook/src/components/bookSearch.vue
+++ soobook/src/components/bookSearch.vue
@@ -17,15 +17,18 @@
               </div>
             <!--App.vue에 있는  results에서 padding, height 값 지움. -->
           </div>
-          <div class="resultsRight">
-            <div class="picked-result" id="div1" @drop="drop($event)" @dragover="allowDrop($event)">
-              <!--App.vue에 있는  picked-result에서 높이값 지움.(이 파일 밑에서 높이값 설정) -->
+          <div class="hiddenArea" style="right: -84%;">
+            <button type="button" class="hiddenMenuButton" @click="lookMenu"><i class="fa fa-angle-double-left fa-2x" aria-hidden="true"></i></button>
+            <div class="resultsRight">
+              <div class="picked-result" id="div1" @drop="drop($event)" @dragover="allowDrop($event)">
+                <!--App.vue에 있는  picked-result에서 높이값 지움.(이 파일 밑에서 높이값 설정) -->
 
+                <router-link to="/mybook">
+                  <button type="button" class="booksave-btn">저장</button>
+                  <!--App.vue에 있는  booksave-btn에서 가운데 정렬하는 값, position 지움. -->
+                </router-link>
+              </div>
             </div>
-            <router-link to="/mybook">
-              <button type="submit" class="booksave-btn">저장</button>
-              <!--App.vue에 있는  booksave-btn에서 가운데 정렬하는 값 지움. -->
-            </router-link>
           </div>
         </div>
 
@@ -36,17 +39,6 @@
 </template>
 
 <script>
-// function drag(target, book) {
-//   book.dataTransfer.setData('Text', target.id);
-// }
-// function drop(target, book) {
-//   var id = book.dataTransfer.getData('Text');
-//   var books = target.appendChild(document.getElementById(id));
-//   console.log('이동된 책 아이디:',id);
-//   selected_booklist.push({"id":id,"cover_thumbnail":book});
-//   var selected_booklist=[];
-// }
-import dragg from './drag.js'
 export default {
   name: 'app',
   data: function () {
@@ -97,13 +89,27 @@ export default {
     drop(ev) {
       ev.preventDefault();
       var data = ev.dataTransfer.getData("drag_target");
-      console.log('data: ', data);
-      console.log(document.getElementById(data));
-      console.log('ev.target:',ev.target);
+      // console.log('data: ', data);
+      // console.log('ev.target:',ev.target);
       var clonebook = document.getElementById(data).cloneNode(true);
-      console.log("clonebook:",clonebook);
+      // console.log("clonebook:",clonebook);
       ev.target.appendChild(clonebook);
       // ev.target.appendChild(document.getElementById(data));
+    },
+    lookMenu(){
+      var hiddenMenu, togglebtn;
+      hiddenMenu = document.querySelector('.hiddenArea').style;
+      if(hiddenMenu.right === "-84%"){
+        hiddenMenu.right = "-10%";
+        hiddenMenu.animation = "toggle-in 0.7s ease-in-out"
+        togglebtn = document.querySelector('.fa-angle-double-left');
+        togglebtn.outerHTML = '<i class="fa fa-angle-double-right fa-2x" aria-hidden="true"></i>';
+      }else{
+        hiddenMenu.right = "-84%";
+        hiddenMenu.animation = "toggle-out 0.7s ease-in-out"
+        togglebtn = document.querySelector('.fa-angle-double-right');
+        togglebtn.outerHTML = '<i class="fa fa-angle-double-left fa-2x" aria-hidden="true"></i>';
+      }
     }
     //  selectBook(e){
     //     var selectBook = document.querySelectorAll('.resultsBookList');
@@ -120,20 +126,27 @@ export default {
 <style>
 .searchResults{
   display: flex;
+  min-height: 600px;
 }
 .results{
   width: 70%;
-  min-height: 500px;
+  max-height: 500px;
   margin-top: 20px;
 }
 .resultsRight{
-  width: 30%;
+  width: 100%;
+  position: relative;
+}
+.booksave-btn{
+  display: block;
+  margin: 0 auto;
 }
 .resultsBookList{
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 22%;
+  height: 270px;
   margin: 10px;
   border:1px solid #ccc;
   /*box-shadow:0 1px 1px;*/
@@ -143,10 +156,19 @@ export default {
 .picked-result{
   min-height: 475px;
   margin-top: 30px;
+  width: 90%;
+}
+.hiddenMenuButton{
+  display: none;
+}
+.hiddenArea{
+  width: 30%;
+  display: flex;
 }
 .picked-result div{
   /*display: inline-block;*/
-  width: 45%;
+  width: 60%;
+  height: 270px;
   margin: 10px auto;
 }
 </style>
