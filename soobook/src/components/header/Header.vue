671b9164030bc1060d2875390fa9922961c13292
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-04-17 16:19:34 +0900

    modify header

--- soobook/src/components/header/Header.vue
+++ soobook/src/components/header/Header.vue
@@ -62,31 +62,12 @@ export default {
                 var tokenKey = response.key;
 
                 // nickname setting
-                // var user_id = response.user.username;
                 var user_nickname = response.user.nickname;
                 console.log(user_nickname);
                 
                 // Cookie setting
                 setCookie('SoobookToken', tokenKey, 90);
                 
-                // function setCookie(name, value, expireDays) {
-                //   var exdate=new Date();
-                //   exdate.setDate(exdate.getDate() + expireDays);
-                //   var c_value=escape(value) + ((expireDays==null) ? "" : "; expires="+exdate.toUTCString());
-                //   document.cookie=name + "=" + c_value;
-                // }
-                // function getCookie(c_name) {
-                // 	var i,x,y,ARRcookies=document.cookie.split(";");
-                // 	for (i=0;i<ARRcookies.length;i++) {
-                // 	  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-                // 	  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-                // 	  x=x.replace(/^\s+|\s+$/g,""); 
-                // 	  if (x==c_name) {
-                // 			return unescape(y);
-                // 		}
-                // 	}
-                // }
-
                 // save key value & nickname to localStorage
                 localStorage.setItem("key", response.key);
                 localStorage.setItem("user", response.user.nickname);
